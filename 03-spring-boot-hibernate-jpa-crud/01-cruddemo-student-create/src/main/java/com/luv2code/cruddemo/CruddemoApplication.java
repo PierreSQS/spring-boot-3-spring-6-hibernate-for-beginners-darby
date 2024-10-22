5a2b9c6ba7c6018460ed4d3ff8dff7538e3dd4e9
PierreSQS <nomo_messina@web.de>
2024-10-22 13:19:36 +0200

    Sec3: optimized code

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+// log messages with lombok doesn't appear on the console
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
