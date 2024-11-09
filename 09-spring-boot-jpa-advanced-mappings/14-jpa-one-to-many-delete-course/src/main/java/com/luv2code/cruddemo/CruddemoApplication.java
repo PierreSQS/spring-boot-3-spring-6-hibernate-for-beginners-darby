47c35fcba63ea05e1c24e5a7fe312e9031c79aa3
PierreSQS <nomo_messina@web.de>
2024-11-09 21:55:45 +0100

    Sec9_Chap312: implemented delete Course in main App

--- 09-spring-boot-jpa-advanced-mappings/14-jpa-one-to-many-delete-course/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/14-jpa-one-to-many-delete-course/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -25,11 +25,50 @@ public class CruddemoApplication {
 
 		return runner -> {
 
-			createInstructorWithCourses(appDAO);
+			// createInstructorWithCourses(appDAO);
 
-			deleteInstructor(appDAO);
+			addNewCourseToInstructor(appDAO);
+
+			// deleteCourse(appDAO);
 
 		};
+	}
+
+	private void deleteCourse(AppDAO appDAO) {
+		int instructID = 12;
+		log.info("Course with ID {}", appDAO.findCourseByID(instructID));
+		log.info("Deleting Course ...");
+		appDAO.deleteCourseByID(instructID);
+		log.info("Course with ID {}", appDAO.findCourseByID(instructID));
+
+	}
+
+
+	private void addNewCourseToInstructor(AppDAO appDAO) {
+		int instructID = 1;
+
+		// find Instructor with ID 1
+		log.info("Finding Instructor with the ID {}",instructID);
+		Instructor foundInstructor = appDAO.findInstructorByIdJoinFetch(instructID);
+		log.info("found Instructor: {}", foundInstructor);
+
+		log.info("Creating new Course...");
+		Course course = new Course();
+		course.setTitle("New Course Title");
+
+		// associate the course with find Instructor
+		log.info("Adding course to Instructor...");
+		foundInstructor.getCourses().add(course);
+		course.setInstructor(foundInstructor);
+
+		// update the Instructor
+		appDAO.updateInstructor(foundInstructor);
+
+		// show the all the Instructor's Courses
+		log.info("All Instructor's Courses: {}",appDAO.findInstructorById(instructID).getCourses());
+
+		printDoneMessage();
+
 	}
 
 	private void deleteInstructor(AppDAO appDAO) {
@@ -53,18 +92,18 @@ public class CruddemoApplication {
 
 	private void createInstructorWithCourses(AppDAO dao) {
 		// create instructor
-		Instructor instructor = new Instructor("Pierrot","ToDelete",
-				"pierrot.todelete@luv2code.com");
+		Instructor instructor = new Instructor("Pierrot","Mongonnam",
+				"pierrot.mongonnam@luv2code.com");
 
 		// create the instructor detail
 		InstructorDetail instructorDetail =
 				new InstructorDetail(
-						"http://www.youtube.com/todelete",
+						"http://www.youtube.com/mongonnam",
 						"Travels");
 
 		// create instructor courses
-		Course course1 = new Course("Java 21");
-		Course course2 = new Course("Spring6");
+		Course course1 = new Course("SpringBoot 3");
+		Course course2 = new Course("Junit5");
 
 		// associate the objects
 		instructor.addCourses(Set.of(course1, course2));
