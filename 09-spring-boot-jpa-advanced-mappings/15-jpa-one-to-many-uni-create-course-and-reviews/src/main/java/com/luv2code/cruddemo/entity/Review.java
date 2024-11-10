c6671789fd5f135ec0d925622254e6bbe21d4424
PierreSQS <nomo_messina@web.de>
2024-11-10 10:18:51 +0100

    Sec9_Chap315: created Review Entity

--- 09-spring-boot-jpa-advanced-mappings/15-jpa-one-to-many-uni-create-course-and-reviews/src/main/java/com/luv2code/cruddemo/entity/Review.java
+++ 09-spring-boot-jpa-advanced-mappings/15-jpa-one-to-many-uni-create-course-and-reviews/src/main/java/com/luv2code/cruddemo/entity/Review.java
@@ -1,4 +1,34 @@
 package com.luv2code.cruddemo.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String comment;
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "comment='" + comment + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
