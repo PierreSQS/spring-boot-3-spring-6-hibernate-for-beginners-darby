fde222fc47cf78f5e6402b8e50a1d225a0136e31
PierreSQS <nomo_messina@web.de>
2024-11-14 10:10:05 +0100

    Sec9_Chap328: implement Find Course and Students in main App

--- 09-spring-boot-jpa-advanced-mappings/19-jpa-many-to-many-find-course-and-students/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/19-jpa-many-to-many-find-course-and-students/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -2,14 +2,11 @@ package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.AppDAO;
 import com.luv2code.cruddemo.entity.Course;
-import com.luv2code.cruddemo.entity.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Set;
 
 @Slf4j
 @SpringBootApplication
@@ -24,48 +21,23 @@ public class CruddemoApplication {
 
 		return runner -> {
 
-			createCoursesAndStudents(appDAO);
+			findCoursesAndStudents(appDAO);
 
 		};
 	}
 
-	private void createCoursesAndStudents(AppDAO appDAO) {
-		// create Course
-		log.info("Creating courses...");
-		Course course = Course.builder().title("Security in Payments Systems").build();
-		log.info("created course {}", course);
+	private void findCoursesAndStudents(AppDAO appDAO) {
+		int courseID = 10;
 
-		// create Students
-		log.info("Creating students...");
-		Student student1 = Student.builder()
-				.firstName("Pierrot")
-				.lastName("Mongonnam")
-				.email("pierrot.mongonnam@gmail.com")
-				.build();
-
-		Student student2 = Student.builder()
-				.firstName("Odile")
-				.lastName("Mongonnam")
-				.email("odile.mongonnam@gmail.com")
-				.build();
-		log.info("Creating courses...");
+		// finding course and students by courseID
+		log.info("Finding course by ID and students...");
+		Course foundCourse = appDAO.findCourseAndReviewsByCourseId(courseID);
 
 
-		log.info("Created Set of Students...");
-		Set<Student> studentSet = Set.of(student1, student2);
-		log.info("Created Set of {} students", studentSet.size());
+		// list the Students to Course
+		log.info("Listing the Students of course with the ID: {}...", courseID);
 
-
-		// Link Students to Course
-		log.info("Linking students to course...");
-		course.setStudents(studentSet);
-
-		// save Course
-		log.info("Saving course...");
-		appDAO.saveCourse(course);
-
-		log.info("Saved the Course {}", course);
-		log.info("The students in the course {}", course.getStudents());
+		log.info("The students in the course {}", foundCourse.getStudents());
 
 
 		printDoneMessage();
