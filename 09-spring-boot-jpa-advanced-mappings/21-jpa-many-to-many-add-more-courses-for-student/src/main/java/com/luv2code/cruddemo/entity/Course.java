8ba3e78e1d327497b84eed3e79573bb3e4e35c71
PierreSQS <nomo_messina@web.de>
2024-11-14 22:23:39 +0100

    Sec9_Chap330-XXX: initial commit

--- 09-spring-boot-jpa-advanced-mappings/21-jpa-many-to-many-add-more-courses-for-student/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/21-jpa-many-to-many-add-more-courses-for-student/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -1,12 +1,32 @@
 package com.luv2code.cruddemo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 @Entity
-@Table(name="course")
 public class Course {
 
     // define our fields
@@ -21,113 +41,46 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
     private int id;
 
-    @Column(name="title")
     private String title;
 
+    // Default FetchType = LAZY
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
                           CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    // Default FetchType = LAZY
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "course_id")
-    private List<Review> reviews;
+    @ToString.Exclude
+    private Set<Review> reviews;
 
-
-    @ManyToMany(fetch = FetchType.LAZY,
-            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+    // Default FetchType = LAZY
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
             CascadeType.DETACH, CascadeType.REFRESH})
-    @JoinTable(
-            name = "course_student",
+    @JoinTable(name = "course_student",
             joinColumns = @JoinColumn(name = "course_id"),
-            inverseJoinColumns = @JoinColumn(name = "student_id")
-    )
-    private List<Student> students;
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    @ToString.Exclude
+    private Set<Student> students;
 
-    public Course() {
-
-    }
-
-    public Course(String title) {
-        this.title = title;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Instructor getInstructor() {
-        return instructor;
-    }
-
-    public void setInstructor(Instructor instructor) {
-        this.instructor = instructor;
-    }
-
-    public List<Review> getReviews() {
-        return reviews;
-    }
-
-    public void setReviews(List<Review> reviews) {
-        this.reviews = reviews;
-    }
-
-    // add a convenience method
-
-    public void addReview(Review theReview) {
-
+    // add convenience methods for Review and Student mapping
+    public void addReview(Review review) {
         if (reviews == null) {
-            reviews = new ArrayList<>();
+            reviews = new HashSet<>();
         }
 
-        reviews.add(theReview);
+        reviews.add(review);
     }
 
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
-
-    // add a convenience method
-
-    public void addStudent(Student theStudent) {
-
+    public void addStudents(Set<Student> studentsToAdd) {
         if (students == null) {
-            students = new ArrayList<>();
+            students = new HashSet<>();
         }
 
-        students.add(theStudent);
+        students.addAll(studentsToAdd);
     }
 
-    @Override
-    public String toString() {
-        return "Course{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                '}';
-    }
 }
-
-
-
-
-
-
