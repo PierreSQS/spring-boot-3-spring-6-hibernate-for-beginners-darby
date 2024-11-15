5431982feba5260745f31281b36fe35e527d4a30
PierreSQS <nomo_messina@web.de>
2024-11-15 17:58:45 +0100

    Sec9_Chap331-XXX: initial commit

--- 09-spring-boot-jpa-advanced-mappings/22-jpa-many-to-many-delete-course/src/main/java/com/luv2code/cruddemo/entity/Review.java
+++ 09-spring-boot-jpa-advanced-mappings/22-jpa-many-to-many-delete-course/src/main/java/com/luv2code/cruddemo/entity/Review.java
@@ -1,64 +1,34 @@
 package com.luv2code.cruddemo.entity;
 
-import jakarta.persistence.*;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-@Table(name="review")
 public class Review {
-
-    // define fields
-
-    // define constructors
-
-    // define getter/setters
-
-    // define toString
-
-    // annotate fields
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
     private int id;
 
-    @Column(name = "comment")
     private String comment;
-
-    public Review() {
-
-    }
-
-    public Review(String comment) {
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
 
     @Override
     public String toString() {
         return "Review{" +
-                "id=" + id +
-                ", comment='" + comment + '\'' +
+                "comment='" + comment + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
-
-
-
-
-
-
