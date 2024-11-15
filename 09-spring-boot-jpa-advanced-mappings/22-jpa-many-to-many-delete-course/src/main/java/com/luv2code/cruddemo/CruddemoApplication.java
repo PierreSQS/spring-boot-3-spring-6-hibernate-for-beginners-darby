7fd32f6ebd490278a1cd57f3f029258446b91f31
PierreSQS <nomo_messina@web.de>
2024-11-15 18:33:57 +0100

    Sec9_Chap331: implemented Delete Course in main App

--- 09-spring-boot-jpa-advanced-mappings/22-jpa-many-to-many-delete-course/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/22-jpa-many-to-many-delete-course/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,15 +1,11 @@
 package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.AppDAO;
-import com.luv2code.cruddemo.entity.Course;
-import com.luv2code.cruddemo.entity.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Set;
 
 @Slf4j
 @SpringBootApplication
@@ -24,40 +20,19 @@ public class CruddemoApplication {
 
 		return runner -> {
 
-			addMoreCoursesForStudent(appDAO);
+			deleteCourse(appDAO);
 
 		};
 	}
 
-	private void addMoreCoursesForStudent(AppDAO appDAO) {
-		int studentID = 1;
-
-		// finding the first student in the Student Set
-		log.info("Finding the first student in the Student Set...");
-		Student foundStudent = appDAO.findStudentAndCoursesByStudentID(studentID);
-
-		log.info("Found Student: {}", foundStudent);
-
-		// list the Students to Course
-		log.info("Listing the courses of student with the ID: {}...", studentID);
-		log.info("The courses of the student before updating: {}", foundStudent.getCourses());
-
-		// Creating New Courses
-		log.info("Creating 2 new courses....");
-		Course course1 = Course.builder().title("New Course1").build();
-		Course course2 = Course.builder().title("New Course2").build();
-
-		// adding courses to student
-		log.info("Adding courses to student...");
-		foundStudent.addCourses(Set.of(course1, course2));
-
-		// updating student
-		log.info("Updating student...");
-		appDAO.updateStudent(foundStudent);
-
-		log.info("the courses of the student after updating: {}",foundStudent.getCourses());
+	private void deleteCourse(AppDAO appDAO) {
+		int instructID = 11;
+		log.info("Finding and Deleting course with ID {}", instructID);
+		appDAO.deleteCourseByID(instructID);
+		log.info("Course with ID {}", appDAO.findCourseByID(instructID));
 
 		printDoneMessage();
+
 	}
 
 	private static void printDoneMessage() {
