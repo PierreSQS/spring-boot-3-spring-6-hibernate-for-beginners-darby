1d2bb1743a96880fe030d04148001f1271c5ba6e
PierreSQS <nomo_messina@web.de>
2024-11-16 22:25:27 +0100

    Sec8_Chap237-XXX: added MVC-Tests

---
+++ 08-spring-boot-spring-mvc-security/03-spring-boot-spring-mvc-security-custom-login-form-plain/src/test/java/com/luv2code/springboot/demosecurity/controller/LoginControllerTest.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import com.luv2code.springboot.demosecurity.security.DemoSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest
+@Import(value = {DemoSecurityConfig.class})
+class LoginControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(get("/showMyLoginPage"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("plain-login.html"))
+                .andExpect(content().string(containsString("<title>Custom Login Page</title>")))
+                .andDo(print());
+    }
+}
