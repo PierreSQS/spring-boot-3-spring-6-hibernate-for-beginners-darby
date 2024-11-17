0e5147dfa87e8a547a43a8eff60dc9dfed98dfc0
PierreSQS <nomo_messina@web.de>
2024-11-17 15:42:12 +0100

    Sec8_Chap243-XXX: added FormLogin-Tests

---
+++ 08-spring-boot-spring-mvc-security/05-spring-boot-spring-mvc-security-custom-login-form-with-bootstrap/src/test/java/com/luv2code/springboot/demosecurity/controller/FormLoginTest.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FormLoginTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void formLoginAuthenticated() throws Exception {
+        mockMvc.perform(formLogin("/authenticateTheUser")
+                        .user("John")
+                        .password("John"))
+                .andExpect(authenticated())
+                .andDo(print());
+    }
+
+    @Test
+    void formLoginNotAuthenticated() throws Exception {
+        mockMvc.perform(formLogin("/authenticateTheUser")
+                        .user("Jean")
+                        .password("XYxxx"))
+                .andExpect(unauthenticated())
+                .andDo(print());
+    }
+}
