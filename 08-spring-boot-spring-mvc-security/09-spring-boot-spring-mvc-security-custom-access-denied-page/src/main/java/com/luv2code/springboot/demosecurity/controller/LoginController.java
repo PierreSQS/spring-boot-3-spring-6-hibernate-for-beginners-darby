ee1a5568d2e80d6efa271ef2c2d4089072cbaa04
PierreSQS <nomo_messina@web.de>
2024-11-18 12:03:11 +0100

    Sec8_Chap254-XXX: initial commit

--- 08-spring-boot-spring-mvc-security/09-spring-boot-spring-mvc-security-custom-access-denied-page/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/09-spring-boot-spring-mvc-security-custom-access-denied-page/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -11,23 +11,4 @@ public class LoginController {
 
         return "fancy-login";
     }
-
-    // add request mapping for /access-denied
-
-    @GetMapping("/access-denied")
-    public String showAccessDenied() {
-
-        return "access-denied";
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
--- 08-spring-boot-spring-mvc-security/09-spring-boot-spring-mvc-security-custom-access-denied-page/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/09-spring-boot-spring-mvc-security-custom-access-denied-page/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -1,36 +1,42 @@
 package com.luv2code.springboot.demosecurity.security;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 public class DemoSecurityConfig {
 
+    public static final String EMPLOYEE_ROLE = "EMPLOYEE";
+    public static final String MANAGER_ROLE = "MANAGER";
+    public static final String ADMIN_ROLE = "ADMIN";
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 
         UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
+                .username("John")
+                .password("{noop}John")
+                .roles(EMPLOYEE_ROLE)
                 .build();
 
         UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
+                .username("Mary")
+                .password("{noop}Mary")
+                .roles(EMPLOYEE_ROLE, MANAGER_ROLE)
                 .build();
 
         UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .username("Susan")
+                .password("{noop}Susan")
+                .roles(EMPLOYEE_ROLE, MANAGER_ROLE, ADMIN_ROLE)
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
@@ -38,27 +44,29 @@ public class DemoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(configurer ->
-                        configurer
-                                .requestMatchers("/").hasRole("EMPLOYEE")
-                                .requestMatchers("/leaders/**").hasRole("MANAGER")
-                                .requestMatchers("/systems/**").hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                )
-                .formLogin(form ->
-                        form
-                                .loginPage("/showMyLoginPage")
-                                .loginProcessingUrl("/authenticateTheUser")
-                                .permitAll()
-                )
-                .logout(logout -> logout.permitAll()
-                )
-                .exceptionHandling(configurer ->
-                        configurer.accessDeniedPage("/access-denied")
-                );
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").hasRole(EMPLOYEE_ROLE)
+                        .requestMatchers("/leaders/**").hasRole(MANAGER_ROLE)
+                        .requestMatchers("/systems/**").hasRole(ADMIN_ROLE)
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser") // allows the authentication automatically
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
