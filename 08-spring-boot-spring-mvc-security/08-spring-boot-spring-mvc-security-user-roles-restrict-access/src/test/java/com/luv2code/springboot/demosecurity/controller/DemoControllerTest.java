1ee5827d904703c921ea3e8d260002948a56729c
PierreSQS <nomo_messina@web.de>
2024-11-18 11:03:11 +0100

    Sec8_Chap251-XXX: added authorization tests

--- 08-spring-boot-spring-mvc-security/08-spring-boot-spring-mvc-security-user-roles-restrict-access/src/test/java/com/luv2code/springboot/demosecurity/controller/DemoControllerTest.java
+++ 08-spring-boot-spring-mvc-security/08-spring-boot-spring-mvc-security-user-roles-restrict-access/src/test/java/com/luv2code/springboot/demosecurity/controller/DemoControllerTest.java
@@ -6,14 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest
+@WebMvcTest(DemoController.class)
 @Import(value = {DemoSecurityConfig.class})
 class DemoControllerTest {
 
@@ -29,6 +33,26 @@ class DemoControllerTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("http://localhost/showMyLoginPage"))
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = "Mary")
+    void showLeaders() throws Exception{
+        mockMvc.perform(get("/leaders"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("leaders"))
+                .andExpect(content().string(containsString("<title>luv2code LEADERS Home Page</title>")))
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = "Susan")
+    void showAdmins() throws Exception{
+        mockMvc.perform(get("/systems"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("systems"))
+                .andExpect(content().string(containsString("<title>luv2code SYSTEMS Home Page</title>")))
                 .andDo(print());
     }
 }
