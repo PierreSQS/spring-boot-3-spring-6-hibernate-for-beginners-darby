cd0d3e21f21b71b1a78c857a2dd52dbf176d2989
PierreSQS <nomo_messina@web.de>
2024-11-19 23:45:07 +0100

    Sec8_Chap269: implemented JDBC Authentication with Customer Tables

--- 08-spring-boot-spring-mvc-security/13-spring-boot-spring-mvc-security-jdbc-bcrypt-custom-tables/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/13-spring-boot-spring-mvc-security-jdbc-bcrypt-custom-tables/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -17,10 +18,21 @@ public class DemoSecurityConfig {
     public static final String EMPLOYEE_ROLE = "EMPLOYEE";
     public static final String MANAGER_ROLE = "MANAGER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String MEMBER_BY_USER_ID = "SELECT user_id, pw, active FROM members WHERE user_id=?";
+    public static final String ROLE_BY_USER_ID = "SELECT user_id, role FROM roles WHERE user_id=?";
 
     @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(MEMBER_BY_USER_ID);
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(ROLE_BY_USER_ID);
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -42,30 +54,6 @@ public class DemoSecurityConfig {
         return http.build();
     }
 }
-
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//
-//        UserDetails john = User.builder()
-//                .username("John")
-//                .password("{noop}John")
-//                .roles(EMPLOYEE_ROLE)
-//                .build();
-//
-//        UserDetails mary = User.builder()
-//                .username("Mary")
-//                .password("{noop}Mary")
-//                .roles(EMPLOYEE_ROLE, MANAGER_ROLE)
-//                .build();
-//
-//        UserDetails susan = User.builder()
-//                .username("Susan")
-//                .password("{noop}Susan")
-//                .roles(EMPLOYEE_ROLE, MANAGER_ROLE, ADMIN_ROLE)
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(john, mary, susan);
-//    }
 
 
 
