e5d027dee9d06674829980ecb3440f1c40a098ac
PierreSQS <nomo_messina@web.de>
2024-11-19 10:31:57 +0100

    Sec8_Chap266-XXX: added Tests

---
+++ 08-spring-boot-spring-mvc-security/12-spring-boot-spring-mvc-security-jdbc-bcrypt/src/test/java/com/luv2code/springboot/demosecurity/controller/FormLoginTest.java
@@ -0,0 +1,80 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FormLoginTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void formLoginAuthenticated() throws Exception {
+        mockMvc.perform(formLogin("/authenticateTheUser")
+                        .user("John")
+                        .password("John"))
+                .andExpect(authenticated())
+                .andDo(print());
+    }
+
+    @Test
+    void formLoginNotAuthenticated() throws Exception {
+        mockMvc.perform(formLogin("/authenticateTheUser")
+                        .user("Jean")
+                        .password("XYxxx"))
+                .andExpect(unauthenticated())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/showMyLoginPage?error"))
+                .andDo(print());
+    }
+
+    @Test
+    void formLogout() throws Exception {
+        mockMvc.perform(logout("/logout"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/showMyLoginPage?logout"))
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = "Susan")
+    void susanAuthenticated() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("User: <span>Susan</span>")))
+                .andExpect(content()
+                        .string(containsString("Role(s): <span>[ROLE_ADMIN, ROLE_EMPLOYEE, ROLE_MANAGER]</span>")))
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = "John")
+    void johnAuthenticated() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("User: <span>John</span>")))
+                .andExpect(content()
+                        .string(containsString("Role(s): <span>[ROLE_EMPLOYEE]</span>")))
+                .andDo(print());
+    }
+
+}
