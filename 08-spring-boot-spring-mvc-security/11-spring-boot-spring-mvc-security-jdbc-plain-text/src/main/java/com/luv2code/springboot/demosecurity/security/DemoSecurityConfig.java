48d36f773cdf97cf81843e0b06b5aacad84900ab
PierreSQS <nomo_messina@web.de>
2024-11-20 00:05:25 +0100

    Sec8_Chap262-XXX: code optimization

--- 08-spring-boot-spring-mvc-security/11-spring-boot-spring-mvc-security-jdbc-plain-text/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/11-spring-boot-spring-mvc-security-jdbc-plain-text/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -19,7 +20,7 @@ public class DemoSecurityConfig {
     public static final String ADMIN_ROLE = "ADMIN";
 
     @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+    public UserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
 
@@ -42,30 +43,6 @@ public class DemoSecurityConfig {
         return http.build();
     }
 }
-
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//
-//        UserDetails john = User.builder()
-//                .username("John")
-//                .password("{noop}John")
-//                .roles(EMPLOYEE_ROLE)
-//                .build();
-//
-//        UserDetails mary = User.builder()
-//                .username("Mary")
-//                .password("{noop}Mary")
-//                .roles(EMPLOYEE_ROLE, MANAGER_ROLE)
-//                .build();
-//
-//        UserDetails susan = User.builder()
-//                .username("Susan")
-//                .password("{noop}Susan")
-//                .roles(EMPLOYEE_ROLE, MANAGER_ROLE, ADMIN_ROLE)
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(john, mary, susan);
-//    }
 
 
 
