f39b4e8a4b3377d5bb259e6b2d67f26c01d82d03
PierreSQS <nomo_messina@web.de>
2024-11-19 10:31:27 +0100

    Sec8_Chap266-XXX: initial commit

--- 08-spring-boot-spring-mvc-security/12-spring-boot-spring-mvc-security-jdbc-bcrypt/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/12-spring-boot-spring-mvc-security-jdbc-bcrypt/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -1,75 +1,77 @@
 package com.luv2code.springboot.demosecurity.security;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+@Slf4j
 @Configuration
 public class DemoSecurityConfig {
 
-    // add support for JDBC ... no more hardcoded users :-)
+    public static final String EMPLOYEE_ROLE = "EMPLOYEE";
+    public static final String MANAGER_ROLE = "MANAGER";
+    public static final String ADMIN_ROLE = "ADMIN";
 
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(configurer ->
-                        configurer
-                                .requestMatchers("/").hasRole("EMPLOYEE")
-                                .requestMatchers("/leaders/**").hasRole("MANAGER")
-                                .requestMatchers("/systems/**").hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                )
-                .formLogin(form ->
-                        form
-                                .loginPage("/showMyLoginPage")
-                                .loginProcessingUrl("/authenticateTheUser")
-                                .permitAll()
-                )
-                .logout(logout -> logout.permitAll()
-                )
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").hasRole(EMPLOYEE_ROLE)
+                        .requestMatchers("/leaders/**").hasRole(MANAGER_ROLE)
+                        .requestMatchers("/systems/**").hasRole(ADMIN_ROLE)
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser") // allows the authentication automatically
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll)
+                // configured the access denied Page
                 .exceptionHandling(configurer ->
-                        configurer.accessDeniedPage("/access-denied")
-                );
+                        configurer.accessDeniedPage("/access-denied") );
 
         return http.build();
     }
-
-    /*
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-    */
-
 }
+
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//
+//        UserDetails john = User.builder()
+//                .username("John")
+//                .password("{noop}John")
+//                .roles(EMPLOYEE_ROLE)
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("Mary")
+//                .password("{noop}Mary")
+//                .roles(EMPLOYEE_ROLE, MANAGER_ROLE)
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("Susan")
+//                .password("{noop}Susan")
+//                .roles(EMPLOYEE_ROLE, MANAGER_ROLE, ADMIN_ROLE)
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+
+
+
+
+
+
+
+
+
