d814fed2694d59dd73ae75add757dc0ba928b6d4
PierreSQS <nomo_messina@web.de>
2024-11-21 06:47:41 +0100

    Sec4_Chap107: implemented Exception Handling for NFE

--- 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,7 +1,12 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import com.luv2code.demo.error.StudentErrorResponse;
+import com.luv2code.demo.exception.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -39,7 +45,23 @@ public class StudentRestController {
 
     @GetMapping("students/{studentID}")
     public Student getStudent(@PathVariable int studentID) {
+        if (studentID > theStudents.size() || studentID < 0)
+            throw new StudentNotFoundException("Student with " +studentID+ " not found !");
         return theStudents.get(studentID);
+    }
+
+    // Add an Exception Handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException sNFE) {
+        // create a ResponseError
+        StudentErrorResponse studentErrResp = new StudentErrorResponse();
+
+        // return the Response Entity
+        studentErrResp.setStatus(HttpStatus.NOT_FOUND.value());
+        studentErrResp.setMessage(sNFE.getMessage());
+        studentErrResp.setTimeStamp(System.currentTimeMillis());
+
+        return ResponseEntity.of(Optional.of(studentErrResp));
     }
 }
 
