e2f5cf5daa4d774d0c469175499d47448c4ff94b
PierreSQS <nomo_messina@web.de>
2024-11-22 08:39:06 +0100

    Sec4_Chap126: implemented REST Get single employee

--- 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,13 @@ public class EmployeeRestController {
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
-    public List<Employee> findAll() {
+    public List<Employee> findAllEmployees() {
         return empServ.findAll();
+    }
+
+    @GetMapping("/employees/{empID}")
+    public Employee findEmployeeByID( @PathVariable int empID) {
+        return empServ.findById(empID);
     }
 
 }
