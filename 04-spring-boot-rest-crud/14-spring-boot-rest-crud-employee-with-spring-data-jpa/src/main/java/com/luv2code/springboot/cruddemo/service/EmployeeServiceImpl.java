2e3dbf098e42cd7b0b2b76d81c2170b202a3b90d
PierreSQS <nomo_messina@web.de>
2024-11-23 00:22:28 +0100

    Sec4_Chap131: adapted Service due to New Spring JPA repository

--- 04-spring-boot-rest-crud/14-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/14-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,41 +1,40 @@
 package com.luv2code.springboot.cruddemo.service;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final EmployeeDAO empDAO;
+    private final EmployeeRepository empRepo;
 
     @Override
     public List<Employee> findAll() {
-        return empDAO.findAll();
+        return empRepo.findAll();
     }
 
     @Override
     public Employee findById(int employeeID) {
-        return Optional.of(empDAO.findById(employeeID))
+        return empRepo.findById(employeeID)
                 .orElseThrow(() -> new RuntimeException("Employee with ID "+employeeID+" not found!"));
     }
 
     @Transactional
     @Override
     public Employee save(Employee employee) {
-        return empDAO.save(employee);
+        return empRepo.save(employee);
     }
 
     @Transactional
     @Override
     public void deleteById(int employeeID) {
-        empDAO.deleteById(employeeID);
+        empRepo.deleteById(employeeID);
     }
 }
 
