619a1133b308bdab0e14d907013b6266d2a6024f
PierreSQS <nomo_messina@web.de>
2024-11-22 07:00:28 +0100

    Sec4_Chap122: refactored EmployeeRestController to use service

--- 04-spring-boot-rest-crud/08-spring-boot-rest-crud-employee-refactored-service-layer/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/08-spring-boot-rest-crud-employee-refactored-service-layer/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,28 +1,25 @@
 package com.luv2code.springboot.cruddemo.rest;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private final EmployeeDAO employeeDAO;
-
-    // quick and dirty: inject employee dao (use constructor injection)
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
-    }
+    private final EmployeeService empServ;
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return empServ.findAll();
     }
 
 }
