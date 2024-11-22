2fd893106ac207e4d1eaf4fd798cc364eb4a8b19
PierreSQS <nomo_messina@web.de>
2024-11-22 12:08:44 +0100

    Sec4_Chap127: implemented REST Add employee

--- 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,8 +3,12 @@ package com.luv2code.springboot.cruddemo.rest;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +30,11 @@ public class EmployeeRestController {
     @GetMapping("/employees/{empID}")
     public Employee findEmployeeByID( @PathVariable int empID) {
         return empServ.findById(empID);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(empServ.save(employee), HttpStatus.CREATED);
     }
 
 }
