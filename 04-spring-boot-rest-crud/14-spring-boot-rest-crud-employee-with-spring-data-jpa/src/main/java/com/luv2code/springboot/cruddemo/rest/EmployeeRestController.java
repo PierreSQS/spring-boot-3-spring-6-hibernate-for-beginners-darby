e1397c477fd5c40f485f01415eecf3b1e348da2b
PierreSQS <nomo_messina@web.de>
2024-11-23 01:37:06 +0100

    Sec4_Chap131-XXX: added the missing handlers

--- 04-spring-boot-rest-crud/14-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/14-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,9 +5,11 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,17 @@ public class EmployeeRestController {
             employee.setId(0);
 
         return new ResponseEntity<>(empServ.save(employee), HttpStatus.CREATED);
+    }
+
+    @PutMapping("employees")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(empServ.update(employee), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/employees/{employeeID}")
+    public ResponseEntity<Void> deleteEmployeeByID(@PathVariable int employeeID) {
+        empServ.deleteById(employeeID);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
