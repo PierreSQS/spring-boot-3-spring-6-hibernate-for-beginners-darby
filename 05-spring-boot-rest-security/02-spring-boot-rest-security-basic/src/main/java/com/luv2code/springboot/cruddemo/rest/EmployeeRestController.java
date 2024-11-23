87e39d7126322e5b65e689f6014603904588c38a
PierreSQS <nomo_messina@web.de>
2024-11-24 00:31:45 +0100

    Sec5_Chap142-XXX: initial commit

--- 05-spring-boot-rest-security/02-spring-boot-rest-security-basic/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 05-spring-boot-rest-security/02-spring-boot-rest-security-basic/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,21 +2,17 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeService employeeService;
-
-    @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService) {
-        employeeService = theEmployeeService;
-    }
+    private final EmployeeService employeeService;
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
@@ -25,21 +21,14 @@ public class EmployeeRestController {
     }
 
     // add mapping for GET /employees/{employeeId}
-
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
 
-        Employee theEmployee = employeeService.findById(employeeId);
+        return employeeService.findById(employeeId);
 
-        if (theEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-
-        return theEmployee;
     }
 
     // add mapping for POST /employees - add new employee
-
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee theEmployee) {
 
@@ -48,9 +37,7 @@ public class EmployeeRestController {
 
         theEmployee.setId(0);
 
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
+        return employeeService.save(theEmployee);
     }
 
     // add mapping for PUT /employees - update existing employee
@@ -58,9 +45,7 @@ public class EmployeeRestController {
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
 
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
+        return employeeService.save(theEmployee);
     }
 
     // add mapping for DELETE /employees/{employeeId} - delete employee
@@ -70,13 +55,7 @@ public class EmployeeRestController {
 
         Employee tempEmployee = employeeService.findById(employeeId);
 
-        // throw exception if null
-
-        if (tempEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-
-        employeeService.deleteById(employeeId);
+        employeeService.deleteById(tempEmployee.getId());
 
         return "Deleted employee id - " + employeeId;
     }
