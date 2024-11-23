e1ce67c643b7bc42b0d4d4f1c8eb0dd8ca01b519
PierreSQS <nomo_messina@web.de>
2024-11-24 00:03:26 +0100

    Sec5_Chap140: initial commit

--- 05-spring-boot-rest-security/01-spring-boot-rest-security-default-security/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security-default-security/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,18 +2,15 @@ package com.luv2code.springboot.cruddemo.service;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
-    @Autowired
     public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
         employeeRepository = theEmployeeRepository;
     }
@@ -25,19 +22,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee findById(int theId) {
-        Optional<Employee> result = employeeRepository.findById(theId);
+        return employeeRepository.findById(theId)
+                .orElseThrow(() -> new RuntimeException("Did not find Employee with ID - " + theId));
 
-        Employee theEmployee = null;
-
-        if (result.isPresent()) {
-            theEmployee = result.get();
-        }
-        else {
-            // we didn't find the employee
-            throw new RuntimeException("Did not find employee id - " + theId);
-        }
-
-        return theEmployee;
     }
 
     @Override
