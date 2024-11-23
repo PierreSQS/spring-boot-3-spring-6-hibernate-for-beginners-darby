f8bf3400ecdf93521119217aa20f7de778cce7e8
PierreSQS <nomo_messina@web.de>
2024-11-23 22:06:33 +0100

    Sec4_Chap137-XXX: initial commit

--- 04-spring-boot-rest-crud/16-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/16-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,9 @@
+// New to Chap131
 package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
