35f62e6120798f130fc07599c309a541b0e4f4b1
PierreSQS <nomo_messina@web.de>
2024-11-24 23:35:13 +0100

    Sec5_Chap150-XXX: disabled CSRF

--- 05-spring-boot-rest-security/04-spring-boot-rest-security-jdbc-plain-text/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/04-spring-boot-rest-security-jdbc-plain-text/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,6 +37,9 @@ public class DemoSecurityConfig {
 
         // Basic Authentication
         http.httpBasic(Customizer.withDefaults());
+
+        // Disable CSRF since API
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
 
