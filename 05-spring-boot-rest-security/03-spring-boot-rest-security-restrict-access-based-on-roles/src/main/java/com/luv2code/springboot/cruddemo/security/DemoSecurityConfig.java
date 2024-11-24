4d6670a2bb7d3519723117c94e704013256e22f9
PierreSQS <nomo_messina@web.de>
2024-11-24 16:52:11 +0100

    Sec5_Chap144: restricted PATH based on roles

--- 05-spring-boot-rest-security/03-spring-boot-rest-security-restrict-access-based-on-roles/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/03-spring-boot-rest-security-restrict-access-based-on-roles/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -12,6 +16,7 @@ public class DemoSecurityConfig {
     public static final String EMPLOYEE_ROLE = "EMPLOYEE";
     public static final String MANAGER_ROLE = "MANAGER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String EMPLOYEE_PATH = "/api/employees";
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
@@ -35,6 +40,23 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, EMPLOYEE_PATH).hasRole(EMPLOYEE_ROLE)
+                .requestMatchers(HttpMethod.GET, EMPLOYEE_PATH+"/**").hasRole(EMPLOYEE_ROLE)
+                .requestMatchers(HttpMethod.POST, EMPLOYEE_PATH).hasRole(MANAGER_ROLE)
+                .requestMatchers(HttpMethod.PUT, EMPLOYEE_PATH).hasRole(MANAGER_ROLE)
+                .requestMatchers(HttpMethod.DELETE, EMPLOYEE_PATH).hasRole(ADMIN_ROLE));
+
+        // Basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
     }
 }
 
