b2a0ec7595b7636dc63754c66fea753e285d6315
PierreSQS <nomo_messina@web.de>
2024-11-24 16:17:00 +0100

    Sec5_Chap144-XXX: initial commit

--- 05-spring-boot-rest-security/03-spring-boot-rest-security-restrict-access-based-on-roles/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/03-spring-boot-rest-security-restrict-access-based-on-roles/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,61 +2,39 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
+
+    public static final String EMPLOYEE_ROLE = "EMPLOYEE";
+    public static final String MANAGER_ROLE = "MANAGER";
+    public static final String ADMIN_ROLE = "ADMIN";
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
+                .username("John")
+                .password("{noop}John")
+                .roles(EMPLOYEE_ROLE)
                 .build();
 
         UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
+                .username("Mary")
+                .password("{noop}Mary")
+                .roles(EMPLOYEE_ROLE, MANAGER_ROLE)
                 .build();
 
         UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .username("Susan")
+                .password("{noop}Susan")
+                .roles(EMPLOYEE_ROLE, MANAGER_ROLE, ADMIN_ROLE)
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-        );
-
-        // use HTTP Basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // disable Cross Site Request Forgery (CSRF)
-        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
     }
 }
 
