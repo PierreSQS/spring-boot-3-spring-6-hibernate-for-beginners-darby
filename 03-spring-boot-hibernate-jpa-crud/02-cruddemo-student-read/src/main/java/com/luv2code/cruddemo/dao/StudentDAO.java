1026012175bdb649526cf063acc86167366fb588
PierreSQS <nomo_messina@web.de>
2024-11-26 22:59:27 +0100

    Sec3_Chap77-XXX: initial commit

--- 03-spring-boot-hibernate-jpa-crud/02-cruddemo-student-read/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/02-cruddemo-student-read/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -1,3 +1,4 @@
+// New to Chap71
 package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
@@ -5,7 +6,5 @@ import com.luv2code.cruddemo.entity.Student;
 public interface StudentDAO {
 
     void save(Student theStudent);
-
-    Student findById(Integer id);
 
 }
--- 03-spring-boot-hibernate-jpa-crud/02-cruddemo-student-read/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/02-cruddemo-student-read/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,32 +2,22 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Repository
 public class StudentDAOImpl implements StudentDAO {
 
     // define field for entity manager
-    private EntityManager entityManager;
-
-    // inject entity manager using constructor injection
-    @Autowired
-    public StudentDAOImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    private final EntityManager entityManager;
 
     // implement save method
     @Override
     @Transactional
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
-    }
-
-    @Override
-    public Student findById(Integer id) {
-        return entityManager.find(Student.class, id);
     }
 
 }
