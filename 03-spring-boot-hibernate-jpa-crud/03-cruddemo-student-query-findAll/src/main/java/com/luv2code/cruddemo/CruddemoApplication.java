6d7cac0ed341480c4b74cee39241808e1fa43911
PierreSQS <nomo_messina@web.de>
2024-11-27 00:13:59 +0100

    Sec3_Chap79: update main App for findAllEmployees

--- 03-spring-boot-hibernate-jpa-crud/03-cruddemo-student-query-findAll/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/03-cruddemo-student-query-findAll/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,11 +1,14 @@
 package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
@@ -18,13 +21,22 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
-		return runner -> queryForStudents(studentDAO);
+		return runner -> findAllEmployees(studentDAO);
 
 }
 
-	private void queryForStudents(StudentDAO studentDAO) {
+	private void findAllEmployees(StudentDAO studentDAO) {
+		log.info("Finding all employees...");
+		List<Student> allEmployees = studentDAO.findAllEmployees();
 
+		log.info("The employees {}", allEmployees);
 
+		doneMessage();
+
+	}
+
+	private static void doneMessage() {
+		log.info("****** Done! ******");
 	}
 
 }
