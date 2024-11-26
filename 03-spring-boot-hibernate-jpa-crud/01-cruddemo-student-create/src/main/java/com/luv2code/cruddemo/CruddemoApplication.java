367b097437e535a75c9b443d55f9ceec8f1a1256
PierreSQS <nomo_messina@web.de>
2024-11-26 20:09:37 +0100

    Sec3_Chap73: updated main App for create student

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -2,11 +2,13 @@ package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.StudentDAO;
 import com.luv2code.cruddemo.entity.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -17,15 +19,26 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
-		return runner -> {
-
-			createStudent(studentDAO);
-
-		};
+		return runner -> createStudent(studentDAO);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
+		// create a student
+		log.info("Creating a student...");
+		Student studentToCreate = Student
+				.builder()
+				.firstName("Paul")
+				.lastName("Doe")
+				.email("paul.doe@luv2code.com")
+				.build();
+		log.info("The created Student {}", studentToCreate);
 
+		// save the new created student
+		log.info("Saving the new created student...");
+		studentDAO.save(studentToCreate);
+
+		// Display the new created Student
+		log.info("Displaying the new created student {}",studentToCreate);
 
 	}
 }
