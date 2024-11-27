2b87fb952213a44a4430adf31164af3a2d6c036a
PierreSQS <nomo_messina@web.de>
2024-11-27 09:32:40 +0100

    Sec3_Chap85: updated main App for deleteAllStudents

--- 03-spring-boot-hibernate-jpa-crud/07-cruddemo-student-query-delete-all-students/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/07-cruddemo-student-query-delete-all-students/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,11 +1,13 @@
 package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.StudentDAO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -21,8 +23,10 @@ public class CruddemoApplication {
 
 	private void deleteAllStudents(StudentDAO studentDAO) {
 
-		System.out.println("Deleting all students");
-//		System.out.println("Deleted row count: " + numRowsDeleted);
+		log.info("Deleting all students...");
+		int numRowsDeleted = studentDAO.deleteAll();
+        log.info("Deleted row count: {}", numRowsDeleted);
+		log.info("***** Done! *****");
 	}
 
 
