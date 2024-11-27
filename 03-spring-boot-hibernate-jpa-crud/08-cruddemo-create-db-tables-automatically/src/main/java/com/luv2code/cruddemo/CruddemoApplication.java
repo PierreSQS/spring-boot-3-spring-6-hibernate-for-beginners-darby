f1631d782b4bd3266894c909ee90420b53762148
PierreSQS <nomo_messina@web.de>
2024-11-27 19:06:56 +0100

    Sec3_Chap87: update main App for createStudents

--- 03-spring-boot-hibernate-jpa-crud/08-cruddemo-create-db-tables-automatically/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/08-cruddemo-create-db-tables-automatically/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -2,6 +2,7 @@ package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.StudentDAO;
 import com.luv2code.cruddemo.entity.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
+@Slf4j
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -21,22 +23,78 @@ public class CruddemoApplication {
 
 		return runner -> {
 			createStudent(studentDAO);
-
 			createMultipleStudents(studentDAO);
-
 		};
 	}
 
-
 	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create students
+		log.info("Creating 3 Students...");
+		Student studentToCreate1 = Student
+				.builder()
+				.firstName("John")
+				.lastName("Doe")
+				.email("john.doe@luv2code.com")
+				.build();
 
+		Student studentToCreate2 = Student
+				.builder()
+				.firstName("Mary")
+				.lastName("Public")
+				.email("john.doe@luv2code.com")
+				.build();
+
+		Student studentToCreate3 = Student
+				.builder()
+				.firstName("Bonita")
+				.lastName("Applebaum")
+				.email("bonita.applebaum@luv2code.com")
+				.build();
+
+		List<Student> students = List.of(studentToCreate1, studentToCreate2, studentToCreate3);
+
+
+		// save the new created students
+		log.info("Saving the new created students...");
+		students.forEach(
+				studentDAO::save
+		);
+
+		// Display the new created Student
+		log.info("Displaying the new created students {}",students);
+
+		// Done message
+		doneMessage();
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
+		// create a student
+		log.info("Creating a student...");
+		Student studentToCreate = Student
+				.builder()
+				.firstName("Paul")
+				.lastName("Doe")
+				.email("paul.doe@luv2code.com")
+				.build();
+		log.info("The created Student {}", studentToCreate);
+
+		// save the new created student
+		log.info("Saving the new created student...");
+		studentDAO.save(studentToCreate);
+
+		// Display the new created Student
+		log.info("Displaying the new created student {}",studentToCreate);
+
+		// Done message
+		doneMessage();
 
 	}
-}
 
+	private static void doneMessage() {
+		log.info("****** Done! ******");
+	}
+
+}
 
 
 
