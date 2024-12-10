c65f1c81a4fc598476ec7942aa23b063993cc652
PierreSQS <nomo_messina@web.de>
2024-12-10 09:07:10 +0100

    Sec6_Chap179-XXX: refactored Student for Lombok

--- 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,8 +1,14 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
 
     private String firstName;
