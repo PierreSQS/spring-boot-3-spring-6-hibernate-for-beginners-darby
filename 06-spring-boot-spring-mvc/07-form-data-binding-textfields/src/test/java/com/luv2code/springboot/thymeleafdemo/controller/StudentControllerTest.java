66e8ce05d67164ed2153678e33cb68e8c913cdef
PierreSQS <nomo_messina@web.de>
2024-12-10 08:37:44 +0100

    Sec6_Chap179-XXX: added a test for the StudentController

---
+++ 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/test/java/com/luv2code/springboot/thymeleafdemo/controller/StudentControllerTest.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void showForm() {
+    }
+
+    @Test
+    void processForm() {
+    }
+}
