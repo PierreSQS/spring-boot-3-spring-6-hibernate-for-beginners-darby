dd989f4c277578acf489da9cd4594465f46d0152
PierreSQS <nomo_messina@web.de>
2024-12-10 08:08:53 +0100

    Sec6_Chap178: step2 - created Controller

--- 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,15 +1,16 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class StudentController {
 
+    // shows the Student form
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -20,15 +21,6 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
 
         return "student-form";
-    }
-
-    @PostMapping("/processStudentForm")
-    public String processForm(@ModelAttribute("student") Student theStudent) {
-
-        // log the input data
-        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
-
-        return "student-confirmation";
     }
 
 }
