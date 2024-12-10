2b3286a70c8ee824302575a2d7b01844959fce21
PierreSQS <nomo_messina@web.de>
2024-12-10 08:35:51 +0100

    Sec6_Chap179: step4 - implemented Form Processing Code

--- 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -21,6 +23,17 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
 
         return "student-form";
+    }
+
+    // shows the Student Confirmation Form
+    // proceeds the Form View Data above
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        // log the input data
+        log.info("theStudent: {} {}", theStudent.getFirstName(), theStudent.getLastName());
+
+        return "student-confirmation";
     }
 
 }
