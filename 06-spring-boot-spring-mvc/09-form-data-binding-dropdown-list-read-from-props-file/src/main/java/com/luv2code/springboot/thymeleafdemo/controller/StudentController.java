4388fd2c683b33849baca5afdf5ae77cb4c2671b
PierreSQS <nomo_messina@web.de>
2024-12-17 00:53:14 +0100

    Sec6_Chap183-XXX: initial commit

--- 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,21 +1,18 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
+@Slf4j
 @Controller
 public class StudentController {
 
-    @Value("${countries}")
-    private List<String> countries;
-
+    // shows the Student form
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -25,17 +22,16 @@ public class StudentController {
         // add student object to the model
         theModel.addAttribute("student", theStudent);
 
-        // add the list of countries to the model
-        theModel.addAttribute("countries", countries);
-
         return "student-form";
     }
 
+    // shows the Student Confirmation Form
+    // proceeds the Form View Data above
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student") Student theStudent) {
 
         // log the input data
-        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        log.info("theStudent: {} {}", theStudent.getFirstName(), theStudent.getLastName());
 
         return "student-confirmation";
     }
