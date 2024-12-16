40e79a7231c1d121595f6c967d8822ede086a628
PierreSQS <nomo_messina@web.de>
2024-12-17 00:08:10 +0100

    Sec6_Chap182: step2-extended Student Class and added lombok

--- 06-spring-boot-spring-mvc/08-form-data-binding-dropdown-list-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/08-form-data-binding-dropdown-list-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,36 +1,18 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
 
-    public Student() {
-
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
 }
