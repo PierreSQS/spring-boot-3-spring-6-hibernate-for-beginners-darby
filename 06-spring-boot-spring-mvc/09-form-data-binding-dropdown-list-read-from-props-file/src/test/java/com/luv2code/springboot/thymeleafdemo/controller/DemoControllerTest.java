c95d41a0f368f42a04a393b40111af1630d27674
PierreSQS <nomo_messina@web.de>
2024-12-17 00:49:46 +0100

    Sec6_Chap183-XXX: added Tests

---
+++ 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/test/java/com/luv2code/springboot/thymeleafdemo/controller/DemoControllerTest.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = {DemoController.class})
+class DemoControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void sayHello() throws Exception {
+        mockMvc.perform(get("/hello?studentName={name}","Pierrot"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("theDate", notNullValue()))
+                .andExpect(view().name("helloworld"))
+                .andExpect(content().string(containsString("Student name: <span >Pierrot</span>")))
+                .andDo(print());
+    }
+}
---
+++ 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/test/java/com/luv2code/springboot/thymeleafdemo/controller/StudentControllerTest.java
@@ -0,0 +1,67 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    Student student1;
+
+    @BeforeEach
+    void setUp() {
+        student1 = Student.builder()
+                .firstName("Test")
+                .lastName("User")
+                .country("BRAZIL")
+                .build();
+    }
+
+    @Test
+    void showForm() throws Exception {
+        mockMvc.perform(get("/showStudentForm"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("student", new Student()))
+                .andExpect(view().name("student-form"))
+                .andExpect(content().string(containsString("<h3>Student Registration Form</h3>")))
+                .andDo(print());
+    }
+
+    @Test
+    void processForm() throws Exception {
+
+        MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add("firstName", student1.getFirstName());
+        multiValueMap.add("lastName", student1.getLastName());
+        multiValueMap.add("country", student1.getCountry());
+
+        mockMvc.perform(post("/processStudentForm").params(multiValueMap)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("student",student1))
+                .andExpect(view().name("student-confirmation"))
+                .andExpect(content()
+                        .string(containsString("The student is confirmed: <span >Test User</span>")))
+                .andExpect(content().string(containsString("Country: <span >BRAZIL</span>")))
+                .andDo(print());
+    }
+}
