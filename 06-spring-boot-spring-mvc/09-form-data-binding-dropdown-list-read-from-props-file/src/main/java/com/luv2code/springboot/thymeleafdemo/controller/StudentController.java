42163751e440c7dd26c16e76717acc8f73d5007d
PierreSQS <nomo_messina@web.de>
2024-12-17 01:23:14 +0100

    Sec6_Chap183: added DropDown List from properties file

--- 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -2,15 +2,21 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countryList;
 
     // shows the Student form
     @GetMapping("/showStudentForm")
@@ -21,6 +27,8 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        theModel.addAttribute("countries", countryList);
 
         return "student-form";
     }
