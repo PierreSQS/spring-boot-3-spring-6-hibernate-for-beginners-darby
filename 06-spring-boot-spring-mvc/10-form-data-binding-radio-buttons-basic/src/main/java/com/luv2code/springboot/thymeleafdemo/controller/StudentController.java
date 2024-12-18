8adfcbfcefb3d138ed4994da76090bb8cdc0371c
PierreSQS <nomo_messina@web.de>
2024-12-18 07:28:12 +0100

    Sec6_Chap185-XXX: initial commit

--- 06-spring-boot-spring-mvc/10-form-data-binding-radio-buttons-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/10-form-data-binding-radio-buttons-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,32 +11,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 public class StudentController {
 
     @Value("${countries}")
-    private List<String> countries;
+    private List<String> countryList;
 
+    // shows the Student form
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
-        // create a student object
+        // create a student object for preloading the Student Form
         Student theStudent = new Student();
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
 
         // add the list of countries to the model
-        theModel.addAttribute("countries", countries);
+        theModel.addAttribute("countries", countryList);
 
         return "student-form";
     }
 
+    // shows the Student Confirmation Form
+    // proceeds the Form View Data above
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student") Student theStudent) {
 
         // log the input data
-        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        log.info("theStudent: {} {}", theStudent.getFirstName(), theStudent.getLastName());
 
         return "student-confirmation";
     }
