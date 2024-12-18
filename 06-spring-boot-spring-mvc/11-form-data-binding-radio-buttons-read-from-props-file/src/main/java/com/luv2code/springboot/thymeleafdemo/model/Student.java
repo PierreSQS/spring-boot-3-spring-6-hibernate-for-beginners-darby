1ebc21bc7f1a8a09677e2290258b01d5254d8cda
PierreSQS <nomo_messina@web.de>
2024-12-18 09:45:38 +0100

    Sec6_Chap186-XXX: initial commit

--- 06-spring-boot-spring-mvc/11-form-data-binding-radio-buttons-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/11-form-data-binding-radio-buttons-read-from-props-file/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,45 +1,18 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
     private String favoriteLanguage;
-
-    public Student() {
-
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getFavoriteLanguage() {
-        return favoriteLanguage;
-    }
-
-    public void setFavoriteLanguage(String favoriteLanguage) {
-        this.favoriteLanguage = favoriteLanguage;
-    }
 }
