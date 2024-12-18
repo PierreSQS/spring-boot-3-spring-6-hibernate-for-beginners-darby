d0c9fbc4740a4ae33a6d1769265f5e73e051f730
PierreSQS <nomo_messina@web.de>
2024-12-18 09:37:51 +0100

    Sec6_Chap186-XXX: added Tests

---
+++ 06-spring-boot-spring-mvc/11-form-data-binding-radio-buttons-read-from-props-file/src/test/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldControllerTest.java
@@ -0,0 +1,53 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void showForm() throws Exception {
+        mockMvc.perform(get("/showForm"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("helloworld-form"))
+                .andExpect(content().string(containsString("<title>Hello World - Input Form</title>")))
+                .andDo(print());
+    }
+
+    @Test
+    void letsShoutDude() throws Exception {
+        mockMvc.perform(get("/processFormVersionTwo")
+                        .param("studentName","Pierrot"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("helloworld"))
+                .andExpect(model().attribute("message", "Yo! PIERROT"))
+                .andExpect(content().string(containsString("Yo! PIERROT")))
+                .andDo(print());
+    }
+
+    @Test
+    void processFormVersionThree() throws Exception {
+        mockMvc.perform(post("/processFormVersionThree")
+                        .param("studentName", "Odile"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("helloworld"))
+                .andExpect(model().attribute("message", "Hey My Friend from v3! ODILE"))
+                .andExpect(content().string(containsString("Odile")))
+                .andDo(print());
+    }
+}
