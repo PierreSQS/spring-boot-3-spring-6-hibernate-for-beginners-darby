aee34cb665c2fd684f5d2401952e0ad1517b7c89
PierreSQS <nomo_messina@web.de>
2024-12-19 09:20:01 +0100

    Sec6_Chap188: introduced Check Boxes

--- 06-spring-boot-spring-mvc/12-form-data-binding-checkboxes-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/12-form-data-binding-checkboxes-basic/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,56 +1,23 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
     private String favoriteLanguage;
+    // introduced in Sec6_Chap188
     private List<String> favoriteSystems;
 
-    public Student() {
-
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getFavoriteLanguage() {
-        return favoriteLanguage;
-    }
-
-    public void setFavoriteLanguage(String favoriteLanguage) {
-        this.favoriteLanguage = favoriteLanguage;
-    }
-
-    public List<String> getFavoriteSystems() {
-        return favoriteSystems;
-    }
-
-    public void setFavoriteSystems(List<String> favoriteSystems) {
-        this.favoriteSystems = favoriteSystems;
-    }
 }
