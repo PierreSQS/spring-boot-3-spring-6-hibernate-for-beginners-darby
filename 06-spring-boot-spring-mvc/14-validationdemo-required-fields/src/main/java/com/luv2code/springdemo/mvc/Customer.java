5416633a59ae52b8b24f2f7b0c0b85a25d7d8ad8
PierreSQS <nomo_messina@web.de>
2024-12-20 00:57:56 +0100

    Sec6_Chap193: step1 - introduced lombok in Entity

--- 06-spring-boot-spring-mvc/14-validationdemo-required-fields/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/14-validationdemo-required-fields/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -2,7 +2,16 @@ package com.luv2code.springdemo.mvc;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+// introduced in Sec6_Chap193
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
 
     private String firstName;
@@ -11,19 +20,4 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
