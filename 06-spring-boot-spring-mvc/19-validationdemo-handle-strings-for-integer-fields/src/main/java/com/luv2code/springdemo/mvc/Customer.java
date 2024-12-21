564242c70bc6d922f25803c70c37c6ac132be7cb
PierreSQS <nomo_messina@web.de>
2024-12-21 04:20:43 +0100

    Sec6_Chap206-XXX: initial commit

--- 06-spring-boot-spring-mvc/19-validationdemo-handle-strings-for-integer-fields/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/19-validationdemo-handle-strings-for-integer-fields/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
 import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class Customer {
 
     private String firstName;
@@ -18,35 +22,4 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public Integer getFreePasses() {
-        return freePasses;
-    }
-
-    public void setFreePasses(Integer freePasses) {
-        this.freePasses = freePasses;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
