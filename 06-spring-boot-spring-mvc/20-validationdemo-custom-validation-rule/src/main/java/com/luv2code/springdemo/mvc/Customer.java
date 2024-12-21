82eb195cc26493fb261bfedca80bfe0dd97e3791
PierreSQS <nomo_messina@web.de>
2024-12-21 23:27:31 +0100

    Sec6_Chap212: added the custom annotation on Entity and introduced lombok

--- 06-spring-boot-spring-mvc/20-validationdemo-custom-validation-rule/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/20-validationdemo-custom-validation-rule/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -2,7 +2,15 @@ package com.luv2code.springdemo.mvc;
 
 import com.luv2code.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
 
     private String firstName;
@@ -19,46 +27,8 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
+    // added in Sec6_Chap212
     @CourseCode(value="TOPS", message="must start with TOPS")
     private String courseCode;
 
-    public String getCourseCode() {
-        return courseCode;
-    }
-
-    public void setCourseCode(String courseCode) {
-        this.courseCode = courseCode;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public Integer getFreePasses() {
-        return freePasses;
-    }
-
-    public void setFreePasses(Integer freePasses) {
-        this.freePasses = freePasses;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
