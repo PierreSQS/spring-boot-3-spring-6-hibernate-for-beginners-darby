b686ec5b8af50c2743a1b968d313444f503aa474
PierreSQS <nomo_messina@web.de>
2024-12-23 06:47:42 +0100

    Sec7_Chap216-XXX: IT-Test code optimization

--- 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-list/src/test/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-list/src/test/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.luv2code.springboot.thymeleafdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ThymeleafdemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
