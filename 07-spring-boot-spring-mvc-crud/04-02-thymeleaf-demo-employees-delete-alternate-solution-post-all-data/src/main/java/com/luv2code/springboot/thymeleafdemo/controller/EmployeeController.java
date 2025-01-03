5665c17b0b1606ad6fe0a24fbf829a6ac43f9a55
PierreSQS <nomo_messina@web.de>
2025-01-03 10:30:35 +0100

    Sec7_SOL04-02: code optimizations

--- 07-spring-boot-spring-mvc-crud/04-02-thymeleaf-demo-employees-delete-alternate-solution-post-all-data/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/04-02-thymeleaf-demo-employees-delete-alternate-solution-post-all-data/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,21 +4,19 @@ import java.util.List;
 
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	private EmployeeService employeeService;
-
-	public EmployeeController(EmployeeService theEmployeeService) {
-		employeeService = theEmployeeService;
-	}
+	private final EmployeeService employeeService;
 
 	// add mapping for "/list"
 
--- 07-spring-boot-spring-mvc-crud/04-02-thymeleaf-demo-employees-delete-alternate-solution-post-all-data/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ 07-spring-boot-spring-mvc-crud/04-02-thymeleaf-demo-employees-delete-alternate-solution-post-all-data/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -1,23 +1,18 @@
 package com.luv2code.springboot.thymeleafdemo.service;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+@RequiredArgsConstructor
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-	private EmployeeRepository employeeRepository;
-	
-	@Autowired
-	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
-		employeeRepository = theEmployeeRepository;
-	}
+	private final EmployeeRepository employeeRepository;
 	
 	@Override
 	public List<Employee> findAll() {
@@ -26,19 +21,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Override
 	public Employee findById(int theId) {
-		Optional<Employee> result = employeeRepository.findById(theId);
-		
-		Employee theEmployee = null;
-		
-		if (result.isPresent()) {
-			theEmployee = result.get();
-		}
-		else {
-			// we didn't find the employee
-			throw new RuntimeException("Did not find employee id - " + theId);
-		}
-		
-		return theEmployee;
+
+		return employeeRepository.findById(theId)
+				.orElseThrow(() -> new RuntimeException("Did not find employee id - " + theId));
 	}
 
 	@Override
