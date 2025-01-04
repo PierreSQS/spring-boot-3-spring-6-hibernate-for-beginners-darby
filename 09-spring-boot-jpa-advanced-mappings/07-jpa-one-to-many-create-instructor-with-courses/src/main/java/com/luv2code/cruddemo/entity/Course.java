be2d5a54b841f3fbfa926ece23c8ff65f6eda1fc
PierreSQS <nomo_messina@web.de>
2025-01-04 06:30:18 +0100

    Sec9_Chap295-XXX: introduced lombok in Entities und DAO

--- 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -1,7 +1,13 @@
 package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@Builder
 @Entity
 @Table(name="course")
 public class Course {
@@ -33,32 +39,10 @@ public class Course {
 
     }
 
-    public Course(String title) {
-        this.title = title;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Course(int id, String title, Instructor instructor) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Instructor getInstructor() {
-        return instructor;
-    }
-
-    public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+        this.title = title;
     }
 
     @Override
--- 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
+++ 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -1,7 +1,20 @@
 package com.luv2code.cruddemo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@Builder
 @Entity
 @Table(name="instructor_detail")
 public class InstructorDetail {
@@ -12,11 +25,7 @@ public class InstructorDetail {
 
     // annotate the fields with db column names
 
-    // create constructors
-
-    // generate getter/setter methods
-
-    // generate toString() method
+    // annotate with Lombok Annotations
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,41 +47,11 @@ public class InstructorDetail {
 
     }
 
-    public InstructorDetail(String youtubeChannel, String hobby) {
-        this.youtubeChannel = youtubeChannel;
-        this.hobby = hobby;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public InstructorDetail(int id, String youtubeChannel, String hobby, Instructor instructor) {
         this.id = id;
-    }
-
-    public String getYoutubeChannel() {
-        return youtubeChannel;
-    }
-
-    public void setYoutubeChannel(String youtubeChannel) {
-        this.youtubeChannel = youtubeChannel;
-    }
-
-    public String getHobby() {
-        return hobby;
-    }
-
-    public void setHobby(String hobby) {
-        this.hobby = hobby;
-    }
-
-    public Instructor getInstructor() {
-        return instructor;
-    }
-
-    public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
     }
 
     @Override
