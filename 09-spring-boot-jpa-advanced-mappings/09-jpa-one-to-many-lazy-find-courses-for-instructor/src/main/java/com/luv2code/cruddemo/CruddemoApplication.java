8ae17ed68a8e65f954000f164af0b61a2e01147d
PierreSQS <nomo_messina@web.de>
2025-01-04 16:07:00 +0100

    Sec9_Chap305-XXX: made the logging more accurate

--- 09-spring-boot-jpa-advanced-mappings/09-jpa-one-to-many-lazy-find-courses-for-instructor/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/09-jpa-one-to-many-lazy-find-courses-for-instructor/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @SpringBootApplication
@@ -28,24 +29,28 @@ public class CruddemoApplication {
 
 	private void findCoursesForInstructor(AppDAO appDAO) {
 
-		int theId = 1;
+		int instructID = 1;
 		// find instructor
-        log.info("### Finding instructor with the ID: {}", theId);
+		log.info("### Finding the Instructor with the ID: {}", instructID);
 
-		Instructor foundInstructor = appDAO.findInstructorById(theId);
+		Instructor instructorById = appDAO.findInstructorById(instructID);
 
-        log.info("### the founded Instructor: {}", foundInstructor);
+		log.info("### The found instructor: {}", instructorById);
 
 		// find courses for instructor
-        log.info("### Finding courses for instructor with ID: {}", theId);
-		List<Course> courses = appDAO.findCoursesByInstructorId(theId);
+		log.info("### Finding the Courses of the Instructor with the ID: {}", instructID);
+		List<Course> coursesByInstructorID = appDAO.findCoursesByInstructorId(instructID);
+		log.info("### The founded courses of the instructor: {}", coursesByInstructorID);
 
 		// WE MUST ASSOCIATE THE OBJECTS, SINCE LAZY LOADING !!!
-		foundInstructor.setCourses(new HashSet<>(courses));
+		instructorById.setCourses(new HashSet<>(coursesByInstructorID));
 
-        log.info("### the associated courses of the Instructor: {}", foundInstructor.getCourses());
+		log.info("### Getting the associated courses of the Instructor with the ID: {}", instructID);
+		// RAISES AN EXCEPTION WITHOUT THE PREVIOUS STATEMENT SINCE LAZY LOADING
+		Set<Course> associatesCourses = instructorById.getCourses();
+		log.info("### the associated courses: {}", associatesCourses);
 
-		log.info("### Done! ###");
+		log.info("Done!");
 	}
 
 }
