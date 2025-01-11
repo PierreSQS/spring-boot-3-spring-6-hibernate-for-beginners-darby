f8f73aa8a7c7e2efb11c558b8ccfe8e386b1c5a1
PierreSQS <nomo_messina@web.de>
2025-01-11 20:24:03 +0100

    Sec3_Chap74-XXX: alternative implementation of the CLR and misc.

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -2,29 +2,29 @@ package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.StudentDAO;
 import com.luv2code.cruddemo.entity.Student;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Slf4j
 @SpringBootApplication
-public class CruddemoApplication {
+public class CruddemoApplication implements CommandLineRunner {
+
+	private final StudentDAO studentDAO;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
-
-		return runner -> {
-			createStudent(studentDAO);
-			createMultipleStudents(studentDAO);
-		};
+	@Override
+	public void run(String... args) {
+		createStudent(studentDAO);
+		createMultipleStudents(studentDAO);
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
@@ -54,14 +54,14 @@ public class CruddemoApplication {
 		List<Student> students = List.of(studentToCreate1, studentToCreate2, studentToCreate3);
 
 
-		// save the new created students
-		log.info("Saving the new created students...");
+		// save the created new students
+		log.info("Saving the created new students...");
 		students.forEach(
 				studentDAO::save
 		);
 
-		// Display the new created Student
-		log.info("Displaying the new created students {}",students);
+		// Display the created new Student
+		log.info("Displaying the created new students {}",students);
 
 		// Done message
 		doneMessage();
@@ -78,11 +78,11 @@ public class CruddemoApplication {
 				.build();
 		log.info("The created Student {}", studentToCreate);
 
-		// save the new created student
-		log.info("Saving the new created student...");
+		// save the created new student
+		log.info("Saving the created new student...");
 		studentDAO.save(studentToCreate);
 
-		// Display the new created Student
+		// Display the created new Student
 		log.info("Displaying the new created student {}",studentToCreate);
 
 		// Done message
