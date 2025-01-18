122cc778ae5f48972e2b45752b59d3be04c96c16
PierreSQS <nomo_messina@web.de>
2025-01-18 14:31:55 +0100

    Sec9_Chap282: step2 - implemented the DAO with lombok

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -2,21 +2,17 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Instructor;
 import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Repository
 public class AppDAOImpl implements AppDAO {
 
     // define field for entity manager
-    private EntityManager entityManager;
-
-    // inject entity manager using constructor injection
-    @Autowired
-    public AppDAOImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    // inject entity manager per lombok
+    private final EntityManager entityManager;
 
     @Override
     @Transactional
