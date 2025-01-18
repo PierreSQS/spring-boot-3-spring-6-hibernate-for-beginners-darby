5e3a3e199444d19f49e2e034d2bf1e15b53d717f
PierreSQS <nomo_messina@web.de>
2025-01-18 14:25:58 +0100

    Sec9_Chap281: created the Instructor Entity with lombok

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -1,7 +1,19 @@
 package com.luv2code.cruddemo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 @Entity
 @Table(name="instructor")
 public class Instructor {
@@ -14,11 +26,7 @@ public class Instructor {
 
     // ** set up mapping to InstructorDetail entity
 
-    // create constructors
-
-    // generate getter/setter methods
-
-    // generate toString() method
+    // add lombok annotations
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,66 +46,6 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    public Instructor() {
-
-    }
-
-    public Instructor(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public InstructorDetail getInstructorDetail() {
-        return instructorDetail;
-    }
-
-    public void setInstructorDetail(InstructorDetail instructorDetail) {
-        this.instructorDetail = instructorDetail;
-    }
-
-    @Override
-    public String toString() {
-        return "Instructor{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", instructorDetail=" + instructorDetail +
-                '}';
-    }
 }
 
 
