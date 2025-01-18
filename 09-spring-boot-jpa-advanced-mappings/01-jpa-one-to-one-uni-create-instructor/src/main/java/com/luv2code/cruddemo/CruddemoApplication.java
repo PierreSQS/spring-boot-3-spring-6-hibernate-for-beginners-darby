7d97c23fc92ecf7b78dd3619a223f8aec709b1bb
PierreSQS <nomo_messina@web.de>
2025-01-18 14:32:32 +0100

    Sec9_Chap283: step3 - refactored the main App with lombok

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -3,11 +3,13 @@ package com.luv2code.cruddemo;
 import com.luv2code.cruddemo.dao.AppDAO;
 import com.luv2code.cruddemo.entity.Instructor;
 import com.luv2code.cruddemo.entity.InstructorDetail;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -18,47 +20,37 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
-		return runner -> {
-			createInstructor(appDAO);
-		};
+		return runner -> createInstructor(appDAO);
 	}
 
 	private void createInstructor(AppDAO appDAO) {
 
-		/*
-		// create the instructor
-		Instructor tempInstructor =
-				new Instructor("Chad", "Darby", "darby@luv2code.com");
+		log.info("creating the instructor and the instructor's details...");
+		Instructor createdInstructor = Instructor.builder()
+				.firstName("Madhu")
+				.lastName("Patel")
+				.email("madhu@luv2code.com").build();
 
 		// create the instructor detail
-		InstructorDetail tempInstructorDetail =
-				new InstructorDetail(
-						"http://www.luv2code.com/youtube",
-						"Luv 2 code!!!");
-		*/
-
-		// create the instructor
-		Instructor tempInstructor =
-				new Instructor("Madhu", "Patel", "madhu@luv2code.com");
-
-		// create the instructor detail
-		InstructorDetail tempInstructorDetail =
-				new InstructorDetail(
-						"http://www.luv2code.com/youtube",
-						"Guitar");
+		InstructorDetail createInstrDetail = InstructorDetail.builder()
+				.youtubeChannel("http://www.luv2code.com/youtube")
+				.hobby("Guitar")
+				.build();
 
 		// associate the objects
-		tempInstructor.setInstructorDetail(tempInstructorDetail);
+		createdInstructor.setInstructorDetail(createInstrDetail);
 
 		// save the instructor
 		//
-		// NOTE: this will ALSO save the details object
+		// NOTE: this will ALSO save the Instructor Details object
 		// because of CascadeType.ALL
 		//
-		System.out.println("Saving instructor: " + tempInstructor);
-		appDAO.save(tempInstructor);
+        log.info("### Saving instructor: {} ... ###", createdInstructor); // instructor doesn't have an ID
+		appDAO.save(createdInstructor);
 
-		System.out.println("Done!");
+		log.info("### The saved Instructor: {} ###", createdInstructor); // instructor new has an ID
+
+		log.info("### Done! ###");
 	}
 }
 
