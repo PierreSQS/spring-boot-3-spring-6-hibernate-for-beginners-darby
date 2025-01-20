1b9a11cc3ea24d9ed1e662abb25b868e847cfe2c
PierreSQS <nomo_messina@web.de>
2025-01-20 14:45:42 +0100

    Sec9_Chap288: added a bidirectional property in the InstructorDetail Entity

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-one-bi-find-instructor-detail/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-one-bi-find-instructor-detail/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.luv2code.cruddemo.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,6 +43,9 @@ public class InstructorDetail {
 
     @Column(name="hobby")
     private String hobby;
+
+    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    private Instructor instructor;
 
 }
 
