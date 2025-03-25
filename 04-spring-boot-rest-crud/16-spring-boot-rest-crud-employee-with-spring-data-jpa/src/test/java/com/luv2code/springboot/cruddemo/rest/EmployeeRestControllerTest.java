916711bd734aaba69f1fdddac8e20efb4a73c5f2
PierreSQS <nomo_messina@web.de>
2025-03-25 12:34:29 +0100

    Sec4_Chap136-XXX: added REST Tests

---
+++ 04-spring-boot-rest-crud/16-spring-boot-rest-crud-employee-with-spring-data-jpa/src/test/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestControllerTest.java
@@ -0,0 +1,190 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import jakarta.servlet.ServletException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeRestController.class)
+class EmployeeRestControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockitoBean
+    EmployeeService employeeService;
+
+    Employee empMock;
+
+    @BeforeEach
+    void setUp() {
+        empMock = Employee.builder()
+                .firstName("Test")
+                .lastName("User")
+                .email("testuser@exampel.com")
+                .build();
+    }
+
+    @Test
+    void findAllEmployees() throws Exception {
+        List<Employee> employees = Arrays.asList(new Employee(), new Employee());
+        given(employeeService.findAll()).willReturn(employees);
+
+        mockMvc.perform(get("/api/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andDo(print());
+    }
+
+    @Test
+    void getEmployeeById() throws Exception {
+        empMock.setId(1);
+        given(employeeService.findById(anyInt())).willReturn(empMock);
+
+        mockMvc.perform(get("/api/employees/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Test"))
+                .andDo(print());
+    }
+
+    @Test
+    void getEmployeeByIdNotFound() {
+        given(employeeService.findById(1)).willReturn(null);
+
+        assertThatThrownBy(() -> mockMvc.perform(get("/api/employees/1")))
+                .isInstanceOf(ServletException.class)
+                .hasMessageContaining("Employee id not found - 1");
+    }
+
+    @Test
+    void addNewEmployee() throws Exception {
+        Employee empToSave = Employee.builder()
+                .firstName(empMock.getFirstName())
+                .lastName(empMock.getLastName())
+                .email(empMock.getEmail())
+                .build();
+
+        empMock.setId(1);
+        given(employeeService.save(any(Employee.class))).willReturn(empMock);
+
+        mockMvc.perform(post("/api/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(empToSave)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.lastName").value("User"))
+                .andDo(print());
+    }
+
+    @Test
+    void updateExistingEmployee() throws Exception {
+        Employee employeeToUpdate = Employee.builder()
+                .id(1)
+                .firstName("Test")
+                .lastName("to update")
+                .email("toupdate@example.com")
+                .build();
+
+        empMock.setId(1);
+
+        given(employeeService.save(any(Employee.class))).willReturn(empMock);
+
+        mockMvc.perform(put("/api/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(employeeToUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.firstName").value("Test"))
+                .andDo(print());
+    }
+
+    @Test
+    void patchEmployeeSuccessfully() throws Exception {
+        empMock.setId(1);
+        given(employeeService.findById(anyInt())).willReturn(empMock);
+        given(employeeService.save(any(Employee.class))).willReturn(empMock);
+
+        Map<String, String> patchPayload = Map.of("firstName", "UpdatedName");
+
+        mockMvc.perform(patch("/api/employees/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patchPayload)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Test"))
+                .andDo(print());
+    }
+
+    @Test
+    void patchEmployeeNotFound() {
+        given(employeeService.findById(anyInt())).willReturn(null);
+
+        Map<String, String> patchPayload = Map.of("firstName", "UpdatedName");
+
+        assertThatThrownBy(() -> mockMvc.perform(patch("/api/employees/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patchPayload))))
+                .isInstanceOf(ServletException.class)
+                .hasMessageContaining("Employee id not found - 1");
+    }
+
+    @Test
+    void patchEmployeeIdNotAllowedToUpdate() {
+        empMock.setId(1);
+        given(employeeService.findById(anyInt())).willReturn(empMock);
+
+        Map<String, String> patchPayload = Map.of("id", "2");
+
+        assertThatThrownBy(() -> mockMvc.perform(patch("/api/employees/2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patchPayload))))
+                .isInstanceOf(ServletException.class)
+                .hasMessageContaining("Employee id is not allowed to be updated");
+    }
+
+    @Test
+    void deleteEmployee() throws Exception {
+        empMock.setId(1);
+        given(employeeService.findById(anyInt())).willReturn(empMock);
+
+        mockMvc.perform(delete("/api/employees/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Deleted employee id - 1"))
+                .andDo(print());
+
+        // verify employeeService.deleteById() is called once
+        verify(employeeService, times(1)).deleteById(1);
+
+
+    }
+
+}
