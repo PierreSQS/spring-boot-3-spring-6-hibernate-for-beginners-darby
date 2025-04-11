45b031a202710eadac8a7105f2653a1dcbfc73f4
PierreSQS <nomo_messina@web.de>
2025-04-11 15:02:56 +0200

    Sec5_Chap154-XXX: init commit

--- 05-spring-boot-rest-security/04-spring-boot-rest-security-restrict-access-based-on-roles-patch/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/04-spring-boot-rest-security-restrict-access-based-on-roles-patch/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -46,7 +46,6 @@ public class DemoSecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
 
