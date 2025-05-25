e49765ae59b7c16fb41ace90c9bde782e86da8b1
PierreSQS <nomo_messina@web.de>
2025-05-25 15:15:06 +0200

    Sec5_Chap163-XXX: optimized MVC-Tests

--- 05-spring-boot-rest-security/05-spring-boot-rest-security-jdbc-plain-text/src/test/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestControllerTest.java
+++ 05-spring-boot-rest-security/05-spring-boot-rest-security-jdbc-plain-text/src/test/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestControllerTest.java
@@ -1,8 +1,9 @@
-// Datei: src/test/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestControllerTest.java
 package com.luv2code.springboot.cruddemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,14 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -25,18 +29,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class EmployeeRestControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objMapper;
 
     @MockitoBean
-    private EmployeeService employeeService;
+    EmployeeService employeeService;
 
+    Employee emp1, emp2;
+
+    @BeforeEach
+    void setUp() {
+        emp1 = new Employee(1, "Max", "Mustermann", "max@firma.de");
+        emp2 = new Employee(2, "Erika", "Musterfrau", "erika@firma.de");
+
+    }
 
     @WithMockUser(username = "max", roles = {"EMPLOYEE"})
     @Test
     void testFindAllEmployees() throws Exception {
-        Employee emp1 = new Employee(1, "Max", "Mustermann", "max@firma.de");
-        Employee emp2 = new Employee(2, "Erika", "Musterfrau", "erika@firma.de");
-        Mockito.when(employeeService.findAll()).thenReturn(Arrays.asList(emp1, emp2));
+
+        given(employeeService.findAll()).willReturn(List.of(emp1, emp2));
 
         mockMvc.perform(get("/api/employees")
                 .contentType(MediaType.APPLICATION_JSON))
@@ -48,8 +62,7 @@ class EmployeeRestControllerTest {
     @WithMockUser(username = "max", roles = {"EMPLOYEE"})
     @Test
     void testGetEmployeeById() throws Exception {
-        Employee emp = new Employee(1, "Max", "Mustermann", "max@firma.de");
-        Mockito.when(employeeService.findById(1)).thenReturn(emp);
+        given(employeeService.findById(1)).willReturn(emp1);
 
         mockMvc.perform(get("/api/employees/1")
                 .contentType(MediaType.APPLICATION_JSON))
@@ -60,44 +73,51 @@ class EmployeeRestControllerTest {
     @WithMockUser(username = "max", roles = {"MANAGER"})
     @Test
     void testAddEmployee() throws Exception {
-        Employee emp = new Employee(0, "Max", "Mustermann", "max@firma.de");
-        Employee savedEmp = new Employee(1, "Max", "Mustermann", "max@firma.de");
-        Mockito.when(employeeService.save(Mockito.any(Employee.class))).thenReturn(savedEmp);
+        Employee empToSave = Employee.builder()
+                .firstName("Max")
+                .lastName("Mustermann")
+                .email("max@firma.de")
+                .build();
+
+        Employee savedEmp = emp1;
+        given(employeeService.save(any(Employee.class))).willReturn(savedEmp);
 
         mockMvc.perform(post("/api/employees")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\":\"Max\",\"lastName\":\"Mustermann\",\"email\":\"max@firma.de\"}"))
+                .content(objMapper.writeValueAsString(empToSave)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1));
+                .andExpect(jsonPath("$.id").value(1))
+                .andDo(print());
     }
 
     @WithMockUser(username = "max", roles = {"MANAGER"})
     @Test
     void testUpdateEmployee() throws Exception {
-        Employee emp = new Employee(1, "Max", "Mustermann", "max@firma.de");
-        Mockito.when(employeeService.save(Mockito.any(Employee.class))).thenReturn(emp);
+        Employee empToUpdate = new Employee(1, "Maximilian", "Mustermann", "maximilian@firma.de");
+        given(employeeService.save(Mockito.any(Employee.class))).willReturn(emp1);
 
         mockMvc.perform(put("/api/employees")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"id\":1,\"firstName\":\"Max\",\"lastName\":\"Mustermann\",\"email\":\"max@firma.de\"}"))
+                .content(objMapper.writeValueAsString(empToUpdate)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1));
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.firstName").value("Max"))
+                .andDo(print());
     }
 
     @WithMockUser(username = "max", roles = {"MANAGER"})
     @Test
     void testPatchEmployee() throws Exception {
-        Employee emp = new Employee(1, "Max", "Mustermann", "max@firma.de");
         Employee patchedEmp = new Employee(1, "Maximilian", "Mustermann", "max@firma.de");
         Map<String, Object> patchPayload = new HashMap<>();
         patchPayload.put("firstName", "Maximilian");
 
-        Mockito.when(employeeService.findById(1)).thenReturn(emp);
-        Mockito.when(employeeService.save(Mockito.any(Employee.class))).thenReturn(patchedEmp);
+        given(employeeService.findById(1)).willReturn(emp1);
+        given(employeeService.save(Mockito.any(Employee.class))).willReturn(patchedEmp);
 
         mockMvc.perform(patch("/api/employees/1")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\":\"Maximilian\"}"))
+                .content(objMapper.writeValueAsString(patchPayload)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.firstName").value("Maximilian"));
     }
@@ -105,8 +125,7 @@ class EmployeeRestControllerTest {
     @WithMockUser(username = "max", roles = {"ADMIN"})
     @Test
     void testDeleteEmployee() throws Exception {
-        Employee emp = new Employee(1, "Max", "Mustermann", "max@firma.de");
-        Mockito.when(employeeService.findById(1)).thenReturn(emp);
+        given(employeeService.findById(1)).willReturn(emp1);
 
         mockMvc.perform(delete("/api/employees/1")
                 .contentType(MediaType.APPLICATION_JSON))
