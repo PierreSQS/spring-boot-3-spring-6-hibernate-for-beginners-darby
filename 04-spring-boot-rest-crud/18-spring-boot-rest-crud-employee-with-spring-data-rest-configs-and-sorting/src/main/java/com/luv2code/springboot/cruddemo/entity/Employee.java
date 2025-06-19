c3ea48f25ddcc259784ad05073384011e495e6e2
PierreSQS <nomo_messina@web.de>
2025-06-19 10:46:17 +0200

    Sec4_Chap142-XXX: Junie refactoring to introduce lombok

--- 04-spring-boot-rest-crud/18-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ 04-spring-boot-rest-crud/18-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -1,9 +1,16 @@
 package com.luv2code.springboot.cruddemo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name="employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class Employee {
 
     // define fields
@@ -21,68 +28,10 @@ public class Employee {
     @Column(name="email")
     private String email;
 
-
-    // define constructors
-    public Employee() {
-
-    }
-
+    // Custom constructor with only required fields (without id)
     public Employee(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
-
-    // define getter/setter
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // define toString
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
-
-
-
-
-
-
-
-
