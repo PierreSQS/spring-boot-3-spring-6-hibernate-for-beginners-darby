d451906f6b89509444a970919797af9f3c50336a
PierreSQS <nomo_messina@web.de>
2025-09-20 23:24:00 +0200

    Sec5_Chap160-XXX: optimized code (SB355)

--- 05-spring-boot-rest-security/04-spring-boot-rest-security-restrict-access-based-on-roles-patch/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 05-spring-boot-rest-security/04-spring-boot-rest-security-restrict-access-based-on-roles-patch/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,25 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper) {
-        employeeService = theEmployeeService;
-        objectMapper = theObjectMapper;
-    }
+    private final ObjectMapper objectMapper;
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
@@ -30,18 +25,12 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
-    // add mapping for GET /employees/{employeeId}
+    // add mapping for GET /employees/{employeeId} - return employee with that id
 
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
 
-        Employee theEmployee = employeeService.findById(employeeId);
-
-        if (theEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-
-        return theEmployee;
+        return employeeService.findById(employeeId);
     }
 
     // add mapping for POST /employees - add new employee
@@ -54,9 +43,7 @@ public class EmployeeRestController {
 
         theEmployee.setId(0);
 
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
+        return employeeService.save(theEmployee);
     }
 
     // add mapping for PUT /employees - update existing employee
@@ -64,9 +51,7 @@ public class EmployeeRestController {
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
 
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
+        return employeeService.save(theEmployee);
     }
 
     // add mapping for PATCH /employees/{employeeId} - patch employee ... partial update
@@ -77,11 +62,6 @@ public class EmployeeRestController {
 
         Employee tempEmployee = employeeService.findById(employeeId);
 
-        // throw exception if null
-        if (tempEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-
         // throw exception if request body contains "id" key
         if (patchPayload.containsKey("id")) {
             throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
@@ -89,9 +69,7 @@ public class EmployeeRestController {
 
         Employee patchedEmployee = apply(patchPayload, tempEmployee);
 
-        Employee dbEmployee = employeeService.save(patchedEmployee);
-
-        return dbEmployee;
+        return employeeService.save(patchedEmployee);
     }
 
     private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
@@ -113,13 +91,7 @@ public class EmployeeRestController {
     @DeleteMapping("/employees/{employeeId}")
     public String deleteEmployee(@PathVariable int employeeId) {
 
-        Employee tempEmployee = employeeService.findById(employeeId);
-
-        // throw exception if null
-
-        if (tempEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
+        employeeService.findById(employeeId);
 
         employeeService.deleteById(employeeId);
 
--- 05-spring-boot-rest-security/04-spring-boot-rest-security-restrict-access-based-on-roles-patch/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/04-spring-boot-rest-security-restrict-access-based-on-roles-patch/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -13,25 +14,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class DemoSecurityConfig {
 
+    public static final String EMP_ROLE = "EMPLOYEE";
+    public static final String MGR_ROLE = "MANAGER";
+
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails john = User.builder()
                 .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
+                .password("{noop}john")
+                .roles(EMP_ROLE)
                 .build();
 
         UserDetails mary = User.builder()
                 .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
+                .password("{noop}mary")
+                .roles(EMP_ROLE, MGR_ROLE)
                 .build();
 
         UserDetails susan = User.builder()
                 .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .password("{noop}susan")
+                .roles(EMP_ROLE, MGR_ROLE, "ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
@@ -42,11 +46,10 @@ public class DemoSecurityConfig {
 
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole(EMP_ROLE)
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole(MGR_ROLE)
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole(MGR_ROLE)
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole(MGR_ROLE)
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
 
@@ -55,7 +58,7 @@ public class DemoSecurityConfig {
 
         // disable Cross Site Request Forgery (CSRF)
         // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(CsrfConfigurer::disable);
 
         return http.build();
     }
