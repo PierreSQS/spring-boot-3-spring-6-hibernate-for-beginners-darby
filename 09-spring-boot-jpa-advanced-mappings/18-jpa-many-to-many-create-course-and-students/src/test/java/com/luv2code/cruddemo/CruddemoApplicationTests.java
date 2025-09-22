15660179f224b9302241ca9b5db91ddeeab7b10b
PierreSQS <nomo_messina@web.de>
2025-09-22 14:46:10 +0200

    Sec9_Chap340-XXX: optimized tests (sb355)

--- 09-spring-boot-jpa-advanced-mappings/18-jpa-many-to-many-create-course-and-students/src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
+++ 09-spring-boot-jpa-advanced-mappings/18-jpa-many-to-many-create-course-and-students/src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.luv2code.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CruddemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
